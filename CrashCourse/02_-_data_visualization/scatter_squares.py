9133505fdf741b2be786503c52a8eaa556dd5800
eflipe <felipecabaleiro@gmail.com>
2020-04-06 12:46:42 -0300

    01 - Matplotlib, subplots, scatter

--- CrashCourse/02_-_data_visualization/scatter_squares.py
+++ CrashCourse/02_-_data_visualization/scatter_squares.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]  # data
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(input_values, squares, s=200)
-
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values,  cmap=plt.cm.Blues, s=10)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-ax.tick_params(axis='both', which='major', labelsize=14)
+plt.savefig('squares_plot.png', bbox_inches='tight')
+ax.axis([0, 1100, 0, 1100000])
+
 plt.show()

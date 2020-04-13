eb0ec49aa78bf4c5800e6baa31a225a6a858b065
eflipe <felipecabaleiro@gmail.com>
2020-04-13 11:24:59 -0300

    09 - Mapa: colores, textos, magbitudes

--- CrashCourse/02_-_data_visualization/eq_world_map.py
+++ CrashCourse/02_-_data_visualization/eq_world_map.py
@@ -2,7 +2,7 @@ import json
 
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -15,17 +15,34 @@ all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
 # print(all_eq_dicts)
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # Mapa
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {
+            'title': 'Magnitude',
+        }
+
+    }
+}]
 my_layout = Layout(title='Terremotos globales')
 
 fig = {'data': data, 'layout': my_layout}

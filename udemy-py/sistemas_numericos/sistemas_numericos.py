7a9ffb4b407e2a26d3072e17dd6f50f05dfb6f44
eflipe <felipecabaleiro@gmail.com>
2021-06-23 19:58:08 -0300

    - bases numericas

---
+++ udemy-py/sistemas_numericos/sistemas_numericos.py
@@ -0,0 +1,16 @@
+a = 10
+
+# constructor init()
+# especificamos la base
+a = int('27', 10)  # base decimal
+a = int('10111', 2)  # base binaria
+a = int('27', 8)  # base octal
+a = int('17', 16)  # base hexa
+print(f'a: {a}')
+a = 3.0
+print(f'a: {a:.5f}')
+a = float(10)
+a = 3e6  # con 6 ceros
+print(f'a: {a}')
+a = 3e-5
+print(f'a: {a:.5f}')

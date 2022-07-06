4b8450dfd87dbf298b97470fad51c0b9aa44e6c7
eflipe <felipecabaleiro@gmail.com>
2022-07-06 14:03:24 -0300

    - conversion de bases

---
+++ udemy-py/29_profundizando_py/conversion_base_numerica.py
@@ -0,0 +1,12 @@
+# base decimal
+a = int('23', 10)
+print(f'Resultado: {a}')
+# base binario
+a = int('10111', 2)
+print(f'Resultado: {a}')
+# base octal
+a = int('27', 8)
+print(f'Resultado: {a}')
+# hexadecimal
+a = int('17', 16)
+print(f'Resultado: {a}')

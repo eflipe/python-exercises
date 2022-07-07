94a14c2258101b2f67500380626e61cebcd5a0e1
eflipe <felipecabaleiro@gmail.com>
2022-07-07 14:57:37 -0300

    - unicode, ascii

---
+++ udemy-py/29_profundizando_py/caracteres_escape.py
@@ -0,0 +1,8 @@
+# \b borra el ultimo caracter
+
+msj = 'Hola Mundo...ppp.\b\b\b'
+print(f'Resultado: {msj}')
+
+# raw string
+resultado = r'Cadena con \n salto de linea pero es raw'
+print(resultado)
---
+++ udemy-py/29_profundizando_py/caracteres_unicode.py
@@ -0,0 +1,14 @@
+# \u unicode
+print('Hola\u0020Mundo')
+print('notacion simple Letra A: ', '\u0041')
+print('notacion extendida Letra A: ', '\U00000041')
+print('notacion hexadecimal Letra A: ', '\x41')
+
+# iconos
+print('Corazon: \u2665')
+
+# ASCII
+caracter = chr(65)
+print(caracter)
+caracter = chr(64)
+print(caracter)

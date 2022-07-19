b7a429f57a5e4c5dda7c77db5721ca66a401d0a3
eflipe <felipecabaleiro@gmail.com>
2022-07-19 17:53:09 -0300

    - lambda y closure

--- udemy-py/43_funciones/funcion_lambda_y_closure.py
+++ udemy-py/43_funciones/funcion_lambda_y_closure.py
@@ -19,3 +19,28 @@ print(mi_funcion_lambda(1, 2, 2, a=1, b=2))  # se pasa un dict de dos elemntos
 # Funciones lambda con argumentos, argumentos variables y valores por default
 mi_funcion_lambda = lambda a, b, c=3, *args, **kwargs: a+b+c+len(args)+len(kwargs)
 print(f'Resultado función lambda: {mi_funcion_lambda(1,2,4,5,6,7,e=5,f=7)}')
+
+
+# Un closure es una función que defina a otra, y además la regresar
+# la función anidada puede acceder a las variables locales definidas
+# en la función principal o externa
+
+# Función principal
+# def operacion(a, b):
+#     # 1. Definimos una función interna o anidada
+#     def sumar():
+#         return a + b
+#
+#     # 2. Retornar la función
+#     return sumar
+
+# Función principal
+def operacion(a, b):
+    # 1. Definimos una función lambda interna o anidada y la retornamos
+    return lambda: a + b
+
+mi_funcion_closure = operacion(5, 2)
+print(f'Resultado de la función closure: {mi_funcion_closure()}')
+
+# Llamar la función regresada al vuelo
+print(f'Resultado de la función closure al vuelo: {operacion(2,3)()}')
